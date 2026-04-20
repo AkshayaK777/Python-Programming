1e06d85d5f7082539b64239099dde17e04dcf54f
Akshaya k <akshayaakshu7880@gmail.com>
2026-04-20 22:48:26 +0530

    Add files via upload

--- grade.py
+++ grade.py
@@ -1,16 +1,14 @@
-marks = float(input("Enter your marks (out of 100): "))
-
-if marks >= 90:
-    grade = "A+"
-elif marks >= 80:
-    grade = "A"
-elif marks >= 70:
-    grade = "B"
-elif marks >= 60:
-    grade = "C"
-elif marks >= 50:
-    grade = "D"
-else:
-    grade = "F"
-
-print("Your Grade is:", grade)
+a = int(input(" enter the marks"))
+if (a>=90):
+    print("a")
+elif ((a>=80) and a<90):
+    print("b")
+elif ((a>=70) and a<80):
+    print("c")
+elif ((a>=60) and a<70):
+    print("d")
+elif ((a>=50) and a<60):
+    print("e")
+elif (a<=40):
+    print("fail")
+                        
