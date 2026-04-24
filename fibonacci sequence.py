5c7f6693bb7cca4bee8bc988228e9ce9cc1a7008
Akshaya k <akshayaakshu7880@gmail.com>
2026-04-24 23:05:55 +0530

    Add files via upload

---
+++ fibonacci sequence.py
@@ -0,0 +1,17 @@
+n_terms = int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+if n_terms <= 0:
+   print("Please enter a positive integer")
+elif n_terms == 1:
+   print("Fibonacci sequence upto", n_terms, ":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < n_terms:
+       print(n1, end="  ")
+       nth = n1 + n2
+       # Update values
+       n1 = n2
+       n2 = nth
+       count += 1
